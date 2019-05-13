6ae014d0b22af47711eb30aaa6f08e43f8b34996
acl <acl447@nyu.edu>
2019-05-12 21:13:59 -0700

    added router to server.js

--- server.js
+++ server.js
@@ -13,3 +13,13 @@ let PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Router
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
+//Starts server listening for client requests
+app.listen(PORT, function() {
+
+    console.log("Server is listening on http://localhost" + PORT);
+});
