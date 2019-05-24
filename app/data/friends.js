e5e46581e935bfd70ae647547468ab32889505d7
acl <acl447@nyu.edu>
2019-05-24 16:21:36 -0700

    changed friendsArray data

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,20 +4,20 @@ let friendsArray = [{
 
     name: "Chelsea",
     photo: "https://scontent.fphx1-2.fna.fbcdn.net/v/t1.0-9/13892328_1326964513999932_654780063439180292_n.jpg?_nc_cat=103&_nc_ht=scontent.fphx1-2.fna&oh=48dfc167028e214343630aa25be66163&oe=5D6540A0",
-    scores: [3, 4, 3, 1, 3, 2, 3, 5, 2, 5]
+    scores: ["3", "4", "3", "1", "3", "2", "3", "5", "2", "5"]
 
 }, 
 
 {
-    name: "Jerome",
-    photo: "https://unsplash.com/photos/veoAiHnM3AI",
-    scores: [5, 1, 2, 3, 4, 2, 2, 3, 1, 5]
+    name: "Jesse",
+    photo: "https://scontent.fphx1-2.fna.fbcdn.net/v/t1.0-9/26731488_10156270939822518_1346522471179191934_n.jpg?_nc_cat=103&_nc_oc=AQnkU1AhfzRqeM-cTN8obJWUXtHf893E2PheoLoRI36AZE_y3jA6fPw-Gm8X602rSiM&_nc_ht=scontent.fphx1-2.fna&oh=8cdc7678ca3b98800800656111bb9279&oe=5D67B0B5",
+    scores: ["5", "1", "2", "3", "4", "2", "2", "3", "1", "5"]
 },
 
 {
-    name: "Kandace",
-    photo: "https://unsplash.com/photos/DrVJk1EaPSc",
-    scores: [1, 1, 2, 4, 2, 3, 4, 1, 2, 3]
+    name: "Hannah",
+    photo: "https://scontent.fphx1-1.fna.fbcdn.net/v/t1.0-9/57226400_2165158083539432_8676189565507076096_n.jpg?_nc_cat=111&_nc_oc=AQmbBc4ZfEv3Wn_4knFQMUjTmayy3wZnK9HqgZmsF0uq_o5VqSHgNvVzgdnFFpPiS6c&_nc_ht=scontent.fphx1-1.fna&oh=8e164d59cc5fc8c94594b105618b3d1d&oe=5D9C3244",
+    scores: ["1", "1", "2", "4", "2", "3", "4", "1", "2", "3"]
 
 }];
 
