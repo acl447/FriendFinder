affb1c387d2253193a2a87ac5b8e5242ffe2ecd4
acl <acl447@nyu.edu>
2019-05-24 16:38:19 -0700

    updated friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,14 +9,14 @@ let friendsArray = [{
 }, 
 
 {
-    name: "Jesse",
-    photo: "https://scontent.fphx1-2.fna.fbcdn.net/v/t1.0-9/26731488_10156270939822518_1346522471179191934_n.jpg?_nc_cat=103&_nc_oc=AQnkU1AhfzRqeM-cTN8obJWUXtHf893E2PheoLoRI36AZE_y3jA6fPw-Gm8X602rSiM&_nc_ht=scontent.fphx1-2.fna&oh=8cdc7678ca3b98800800656111bb9279&oe=5D67B0B5",
+    name: "Bill",
+    photo: "http://itu150.org/wp-content/uploads/2015/05/gates-web-500x500.jpg",
     scores: ["5", "1", "2", "3", "4", "2", "2", "3", "1", "5"]
 },
 
 {
     name: "Hannah",
-    photo: "https://scontent.fphx1-1.fna.fbcdn.net/v/t1.0-9/57226400_2165158083539432_8676189565507076096_n.jpg?_nc_cat=111&_nc_oc=AQmbBc4ZfEv3Wn_4knFQMUjTmayy3wZnK9HqgZmsF0uq_o5VqSHgNvVzgdnFFpPiS6c&_nc_ht=scontent.fphx1-1.fna&oh=8e164d59cc5fc8c94594b105618b3d1d&oe=5D9C3244",
+    photo: "https://inspirationalwomenseries.files.wordpress.com/2017/12/aaeaaqaaaaaaaai_aaaajdm1mmjmotvkltq1mdatndu4mi04yzrjlwm4ndjkymvmody0na.jpg?w=500&h=500&crop=1",
     scores: ["1", "1", "2", "4", "2", "3", "4", "1", "2", "3"]
 
 }];
